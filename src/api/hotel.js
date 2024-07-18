c41e3818c5f6da7b11a5e3d4cce19d19deff2749
yemyokyaw <yemyokyaw.etp@gmail.com>
2024-07-18 10:10:40 +0630

    conflict fix

--- src/api/hotel.js
+++ src/api/hotel.js
@@ -17,13 +17,3 @@ export const getAvailableHotels = async (payload) => {
     return error.response;
   }
 };
-import { api } from "../config/axios";
-
-export const getAllHotels = async () => {
-  try {
-    const response = await api.get("/api/hotels/all");
-    return response;
-  } catch (error) {
-    return error.response;
-  }
-};
